883ab69568d796582ba1c64674ba913c0b2280df
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-10-29 14:21:45 +1300

    a

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function changefont() {
         document.body.style.fontFamily = 'Courier New';
         document.body.style.color = "#000";
         document.body.style.backgroundColor = "#FFF";
-        for (let i = document.getElementsByClassName("visible").length - 1; i < document.getElementsByClassName("visible").length; i++) {
+        for (let i = 0; i < document.getElementsByClassName("visible").length; i++) {
             document.getElementsByClassName("visible")[i].style.backgroundColor = "#FFF";
         }
         document.getElementById("title").style.backgroundColor = "#FFF"
@@ -15,7 +15,7 @@ function changefont() {
         document.body.style.fontFamily = 'Helvetica';
         document.body.style.color = "#9b2948";
         document.body.style.backgroundColor = "#ffedbf";
-        for (let i = document.getElementsByClassName("visible").length - 1; i < document.getElementsByClassName("visible").length; i++) {
+        for (let i = 0; i < document.getElementsByClassName("visible").length; i++) {
             document.getElementsByClassName("visible")[i].style.backgroundColor = "#ffca7b";
         }
         document.getElementById("title").style.backgroundColor = "#ffca7b"
