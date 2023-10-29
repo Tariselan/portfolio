96454a2fea636cc240785d56d8d389ae001aac1f
krv1974 <63528758+krv1974@users.noreply.github.com>
2023-10-29 13:57:50 +1300

    accessibility hoorah!

---
+++ script.js
@@ -0,0 +1,24 @@
+var accessmode = false
+
+function changefont() {
+    if (accessmode == false) {
+        document.body.style.fontFamily = 'Courier New';
+        document.body.style.color = "#000";
+        document.body.style.backgroundColor = "#FFF";
+        for (let i = document.getElementsByClassName("visible").length - 1; i < document.getElementsByClassName("visible").length; i++) {
+            document.getElementsByClassName("visible")[i].style.backgroundColor = "#FFF";
+        }
+        document.getElementById("title").style.backgroundColor = "#FFF"
+        accessmode = true;
+    }
+    else {
+        document.body.style.fontFamily = 'Helvetica';
+        document.body.style.color = "#9b2948";
+        document.body.style.backgroundColor = "#ffedbf";
+        for (let i = document.getElementsByClassName("visible").length - 1; i < document.getElementsByClassName("visible").length; i++) {
+            document.getElementsByClassName("visible")[i].style.backgroundColor = "#ffca7b";
+        }
+        document.getElementById("title").style.backgroundColor = "#ffca7b"
+        accessmode = false;
+    }
+}
